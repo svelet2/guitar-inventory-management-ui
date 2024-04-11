1713e4c4402f919b6c32b2cd10afb2faf737d9eb
Sasha <svelet@iu.edu>
2024-04-11 15:45:23 -0400

    merged with hw8

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 0;
 
 const host_local = "http://localhost:8080";
-const host_remote = "";
+const host_remote = "https://guitar-inventory-management-svelet2.onrender.com";
 
 function getHost() {
     return (mode == 0) ? host_local : host_remote;
